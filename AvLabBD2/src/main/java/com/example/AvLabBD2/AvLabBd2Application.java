8479128aeb147471cb713d423fde001fb76966fc
Pedro Noboru Nakao Noronha <nakPedro7@gmail.com>
2026-05-01 17:38:51 -0300

    Add files via upload

---
+++ AvLabBD2/src/main/java/com/example/AvLabBD2/AvLabBd2Application.java
@@ -0,0 +1,13 @@
+package com.example.AvLabBD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvLabBd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvLabBd2Application.class, args);
+	}
+
+}
---
+++ AvLabBD2/src/test/java/com/example/AvLabBD2/AvLabBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AvLabBD2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvLabBd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
